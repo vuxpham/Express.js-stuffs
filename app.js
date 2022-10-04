b271ae2b89f4eb43c6b518f9ed3a4c78e3704c6b
vuxpham <phamanhvu281003@gmail.com>
2022-10-04 14:13:23 +0700

    use() and next() functions

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const express = require('express');
 
 const app = express();  //create express object. Object is also a valid parameter for create server
 
-app.use((req, res, next)=>{              //Middleware
-	console.log("In middleware");
-	next();                              //next() is required to move to the next middleware
+//5 overloads of use() function, read doc to learn more
+
+app.use('/', (req, res, next)=>{
+	/......                               this code always runs
+	next();                             //next() to move to next middleware after this
 });
 
-app.use((req, res, next)=>{              //Another middleware
-	console.log("In another middleware");
-	res.send('<h1>Hello</h1>');
+
+app.use('/add-product', (req, res, next)=>{ //this runs first   
+	res.send('<h1>Add Produc Page</h1>');   //Handles request for route that starts with '/add-product'        
+});                                         //Does not have next() so the middleware under does not execute after this. 
+
+
+//Generally don't want next() after code that writes responses, like no writing after end(). 
+
+app.use('/', (req, res, next)=>{            //handle request for every route that starts with '/'  
+	res.send('<h1>Hello</h1>');             
 });
 
-app.listen(3000);                        //the app function intializes a server object and passes in app function -> no need to create
-                                         //server ourselves
+app.listen(3000);                        
