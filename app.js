70599f04106ea5451602b603510df87643b002d3
vuxpham <phamanhvu281003@gmail.com>
2022-10-07 08:48:59 +0700

    Added error page and filtering routes

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));  
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);   //only addresses with '/admin' can run this code
 
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>');     //Error page 
+});
 
 app.listen(3000);                        
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router(); //A mini app
 
 router.get('/add-product', (req, res, next)=>{        
-	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');      
+	res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');      
 });                            
 
-router.post('/product', (req, res, next)=>{
-	console.log(req.body);                          
+router.post('/add-product', (req, res, next)=>{  //Can have same address because
+	console.log(req.body);                             //2 code handles different requests
 	                                                
 	res.redirect('/');                              
 });
