4e7e0d36af2e1097d18f2ad64b7b90390f2df911
vuxpham <phamanhvu281003@gmail.com>
2022-10-10 08:36:19 +0700

    Added changes to css and html files, also added saving user input on the server side

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,21 @@ const path = require('path');
 
 const rootDir = require('../utility/path');
 
-const router = express.Router(); //A mini app
+const router = express.Router(); 
+
+const products = [];            
 
 router.get('/add-product', (req, res, next)=>{        
 	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));      
 });                            
 
-router.post('/add-product', (req, res, next)=>{  //Can have same address because
-	console.log(req.body);                             //2 code handles different requests
-	                                                
-	res.redirect('/');                              
+router.post('/add-product', (req, res, next)=>{  
+	products.push({title: req.body.title});        //add user form input to the products array                           
+	                                               //The products added to the array will be saved on server side 
+	res.redirect('/');                             // => User B can see what user A logged. 
 });
 
 
-module.exports = router;
+exports.routes = router;          //exports is an object, we give exports property routes = router
+								  //property products = products
+exports.products = products;       
